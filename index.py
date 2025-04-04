c68c6f730d316118b0191c5fe6ba67e33ca33bbe
Michael Smith <michael.smith.ok@gmail.com>
2025-04-04 09:29:45 -0500

    Allow changing extension, and include time stamps

--- index.py
+++ index.py
@@ -3,10 +3,12 @@ from os import environ
 from time import sleep
 
 import whisper
+from whisper.utils import get_writer
 
 search_path = Path(environ.get('SEARCH_PATH', '/files'))
 wait_time = int(environ.get('INTERVAL', '1800'))
 file_extensions = environ.get('FILE_EXTENSIONS', 'mp3')
+target_file_extension = environ.get('TARGET_FILE_EXTENSION', 'md')
 
 file_extensions = [".{}".format(f) for f in file_extensions.split(',')]
 model = whisper.load_model('large')
@@ -14,6 +16,11 @@ model = whisper.load_model('large')
 def transcribe(audio_file_path, target_file_path):
     result = model.transcribe(audio_file_path)
 
+    target_path = Path(target_file_path)
+    target_path.resolve()
+
+    writer = get_writer('tsv', str(target_path.parent))
+    writer(result, str(target_path.name))
     with open(target_file_path, 'w') as f:
         f.write(result["text"])
 
@@ -26,7 +33,7 @@ while True:
                 continue
             file.resolve()
             source_file_path = str(file)
-            target_file_path = source_file_path + '.txt'
+            target_file_path = '{}.{}'.format(source_file_path, target_file_extension)
             if Path(target_file_path).exists():
                 continue
             print("Transcribing {}".format(source_file_path))
